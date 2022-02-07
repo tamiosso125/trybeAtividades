ae28fbae174ad45e675c1ceba8e4a3f0be5dbbb6
Guilherme Tamiosso <guilherme.tamiosso@hotmail.com>
2022-02-07 13:53:25 -0500

    Atividades 1-4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/java.js
@@ -0,0 +1,17 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+// console.log("Bem Vinda, " + info.personagem);
+
+// console.log(info.recorrente);
+
+// for(let index in info){
+//   console.log(index);
+// }
+// for(let index in info){
+//   console.log(info[index]);
+// }
