aaada8907ca4e1b1014aa714c58566c02e38c2c5
Guilherme Tamiosso <guilherme.tamiosso@hotmail.com>
2022-03-11 09:30:43 -0500

    Atividades

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script.js
@@ -0,0 +1,101 @@
+const books = [
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// const verifyEven = (number) => number % 2 === 0;
+
+// const isEven = numbers.find(verifyEven);
+
+// console.log(isEven); // 30
+
+// console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
+
+// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
+// const isEven2 = numbers.find((number) => number % 2 === 0);
+
+// console.log(isEven2); // 30
+
+// function authorBornIn1947() {
+//   // escreva aqui o seu código
+//   return books.find((elements) => elements.author.birthYear === 1947).author.name;
+
+// }
+// console.log(authorBornIn1947());
+
+// function smallerName() {
+//   let nameBook;
+//   // escreva aqui o seu código
+//   books.forEach((elements) => {
+//     if (!nameBook|| elements.name.length < nameBook.length) {
+//       nameBook = elements.name;
+//     }
+//   });
+//   // Variável nameBook que receberá o valor do menor nome;
+
+//   return nameBook;
+// }
+
+// console.log(smallerName());
+
